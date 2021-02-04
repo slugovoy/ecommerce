61e9827a57e3a82c082eae6e6942cefcd16d610f
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-03 19:43:40 -0700

    signout route

--- controllers/user.js
+++ controllers/user.js
@@ -9,7 +9,7 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: errorHandler(err)
+        err: errorHandler(err),
       });
     }
     user.salt = undefined;
@@ -21,31 +21,34 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-
   //  find the user based on email
 
-  const {email, password} = req.body;
-  User.findOne({ email}, (err, user) => {
-    if(err || !user) {
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
       return res.status(400).json({
-        error: "User with that email does not exist. Please signup"
-      })
+        error: "User with that email does not exist. Please signup",
+      });
     }
     // if user is found make sure the email and password match
     // create authenticate method in user model
-    if(!user.authenticate(password)) {
+    if (!user.authenticate(password)) {
       return res.status(401).json({
-        error: "Email and password don't match"
-      })
+        error: "Email and password don't match",
+      });
     }
 
     // generate a signed token with user id and secret
-    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
     // persist the token as 't' in cookie with expiry date
-    res.cookie('t', token, { expire: new Date() + 9999})
+    res.cookie("t", token, { expire: new Date() + 9999 });
     // return response with user and token to frontend client
-    const {_id, name, email, role} = user;
-    return res.json({token, user: {_id, email, name, role}});
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
 
-  })
-}
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout success" });
+};
