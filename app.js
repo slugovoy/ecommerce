3b2da3de0d8cb8c1163d2d16289b8458ac3ba870
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-07 21:11:53 -0700

    installed package

--- app.js
+++ app.js
@@ -1,52 +1,49 @@
-const express = require('express');
-require('dotenv').config();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const expressValidator = require('express-validator');
-
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
 
 // import routes
 
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
 // App
 const app = express();
 
 // DB
-mongoose.connect(
-    process.env.MONGODB_URI,
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }
-  )
-  .then(() => console.log('DB Connected'))
-   
-  mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`)
-  });
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 // Middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(expressValidator())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
 
 //  Routes middleware
-app.use('/api', authRoutes)
-app.use('/api', userRoutes)
-app.use('/api', categoryRoutes)
-app.use('/api', productRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
