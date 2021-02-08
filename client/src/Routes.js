e566b60f1ad92964e1241c8d39d03b8f75b960d2
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-08 10:59:45 -0700

    routes

---
+++ client/src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Signin from './user/Signin';
+import Signup from './user/Signup';
+import Home from './core/Home';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+            <Route exact path="/" component={Home} />
+                <Route exact path="/signin" component={Signin} />
+                <Route exact path="/signup" component={Signup} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
