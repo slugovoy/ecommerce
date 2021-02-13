1896e4ecb58cbd8de27c49558e0c5eb8ddc29d80
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-12 22:13:48 -0700

    shop page start

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -7,14 +7,16 @@ import PrivateRoute from "./auth/PrivateRoute";
 import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
 import AdminDashboard from "./user/AdminDashboard";
-import AddCategory from "./admin/AddCategory"
-import AddProduct from "./admin/AddProduct"
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
+import Shop from "./core/Shop";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
         <PrivateRoute exact path="/user/dashboard" component={Dashboard} />
---
+++ client/src/core/Shop.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import Card from "./Card";
+import { getCategories } from "./apiCore";
+
+const Shop = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <Layout
+      title="Shop Page"
+      description="Search and find books of your choice"
+      className="container-fluid"
+    >
+      <div className="row">{JSON.stringify(categories)}</div>
+
+      <div className="col-8">Right</div>
+    </Layout>
+  );
+};
+
+export default Shop;
