0720039420fbd59bdbde6f3533ecc2d912d24196
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-12 20:17:28 -0700

    added product by sell and arrival

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 const Home = () => {
+  const [productsBySell, setProductBySell] = useState([]);
+  const [productsByArrival, setProductByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductBySell(data);
+      }
+    });
+  };
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
     <Layout title="Home Page" description="Node React E-Commerce App">
-      ...
+      {JSON.stringify(productsByArrival)}
+      <hr />
+      {JSON.stringify(productsBySell)}
     </Layout>
   );
 };
