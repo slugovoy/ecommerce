33b41202b19c61f1a82297b183ec3688e82191fb
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-13 20:57:54 -0700

    list of categories and checkboxes

---
+++ client/src/core/Checkbox.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const Checkbox = ({ categories }) => {
+  return categories.map((category, index) => (
+    <li key={index} className="list-unstyled">
+      <input type="checkbox" className="form-check-input" />
+      <label className="form-check-label">{category.name}</label>
+    </li>
+  ));
+};
+
+
+export default Checkbox;
--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
 
 const Shop = () => {
   const [categories, setCategories] = useState([]);
@@ -27,9 +28,15 @@ const Shop = () => {
       description="Search and find books of your choice"
       className="container-fluid"
     >
-      <div className="row">{JSON.stringify(categories)}</div>
-
-      <div className="col-8">Right</div>
+      <div className="row">
+        <div className="col-4">
+          <h4>Filter by Categories</h4>
+          <ul>
+            <Checkbox categories={categories} />
+          </ul>
+        </div>
+        <div className="col-8">Right</div>
+      </div>
     </Layout>
   );
 };
