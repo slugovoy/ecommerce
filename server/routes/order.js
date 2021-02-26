604653000fdaa52c35c779c573b41181ad95d454
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-02-25 21:52:09 -0700

    order's history

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const { requireSignin, isAuth } = require("../controllers/auth");
-const { userById } = require("../controllers/user");
+const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { create } = require("../controllers/order");
 
-router.post("/order/create/:userId", requireSignin, isAuth, create);
+router.post(
+  "/order/create/:userId",
+  requireSignin,
+  isAuth,
+  addOrderToUserHistory,
+  create
+);
 
-router.param("userId", userById)
+router.param("userId", userById);
 
-module.exports = router
+module.exports = router;
